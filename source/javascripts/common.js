ca137889639a56aa05cf8a2b3618bdf95a64386c
vasiliy <vasilyator@gmail.com>
2013-08-15 02:08:48 +0400

    Прикручен датапикер, но зачем.

--- source/javascripts/common.js
+++ source/javascripts/common.js
@@ -3,11 +3,38 @@ $(function() {
     var template = $('#question_template').html();
     var html = Mustache.to_html(template, data);
     $('.questions').html(html);
+      $(function(){
+          $(".datepick").datepicker();
+      });
   });
   $('#q-form').validate();
 });
 $(document).ready(function(){
-
     $("span.tester").addClass("second");
 
+
 })
+
+$(function(){
+    $(".datepick").datepicker();
+});
+
+
+
+jQuery.extend(jQuery.validator.messages, {
+    required: "Это поле обязательно для заполнения.",
+    remote: "Исправте это поле.",
+    email: "Введите корhектный e-male адрес.",
+    url: "Введите корректный URL.",
+    date: "Введите корректную дату.",
+    number: "Введите число.",
+    digits: "Используйте только цифры.",
+    equalTo: "Please enter the same value again.",
+    accept: "Please enter a value with a valid extension.",
+    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
+    minlength: jQuery.validator.format("Please enter at least {0} characters."),
+    rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
+    range: jQuery.validator.format("Please enter a value between {0} and {1}."),
+    max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
+    min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
+});
