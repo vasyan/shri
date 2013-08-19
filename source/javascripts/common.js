644fd896776a6b502a36542deb17c159a43ec77a
vasiliy <vasilyator@gmail.com>
2013-08-19 14:31:17 +0400

    validation success

--- source/javascripts/common.js
+++ source/javascripts/common.js
@@ -14,6 +14,8 @@ $(function() {
     });
   });
 
+
+
     $(function () {
         $.validator.addMethod("valueNotEquals", function(value, element, arg){
             return arg != value;
@@ -33,10 +35,22 @@ $(function() {
                         minlength: 2,
                         required: true
                     },
+                    recommend: {
+                        valueNotEquals: "default"
+                    },
                     message: {
                         minlength: 2,
                         required: true
-                    }
+                    },
+                    agree: "required"
+
+            },
+            messages: {
+                    agree: {
+                        required: "Согласитесь на все."
+                    },
+                    email: "Введите ваш email.",
+                    recommend: "Выберите один из вариантов."
             },
             highlight: function (element) {
                 $(element).closest('.control-group').removeClass('success').addClass('error');
@@ -73,24 +87,16 @@ $(function() {
 });
 
 
-
 jQuery.extend(jQuery.validator.messages, {
-    required: "Это поле обязательно.",
+    required: "Заполните это поле.",
     remote: "Исправте это поле.",
     email: "E-mail введен не верно.",
     url: "Введите корректный URL.",
     date: "Введите корректную дату.",
     number: "Введите число.",
     digits: "Используйте только цифры.",
-    equalTo: "Please enter the same value again.",
-    accept: "Please enter a value with a valid extension.",
-    maxlength: jQuery.validator.format("Please enter no more than {0} characters."),
-    minlength: jQuery.validator.format("Please enter at least {0} characters."),
-    rangelength: jQuery.validator.format("Please enter a value between {0} and {1} characters long."),
-    range: jQuery.validator.format("Please enter a value between {0} and {1}."),
-    max: jQuery.validator.format("Please enter a value less than or equal to {0}."),
-    min: jQuery.validator.format("Please enter a value greater than or equal to {0}.")
 });
+
 
 
 
